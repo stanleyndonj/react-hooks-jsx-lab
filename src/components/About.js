62251491e223252941a10793ecfd9470cfd308a6
stanleyndonj <stanley.ndonj@student.moringaschool.com>
2024-08-16 15:57:13 +0300

    lab completed

--- src/components/About.js
+++ src/components/About.js
@@ -3,8 +3,8 @@ import { image } from "../data/data";
 
 function About() {
   return <div id="about">
-        <h2>About me</h2>
-        <p>Yes this is me</p>
+        <h2>About Me</h2>
+        <p>Yes this is me and i am a certified software developer </p>
         <img src={image} alt="I made this" />
   </div>;
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,11 +1,12 @@
 import React from "react";
 
+
 function NavBar() {
-  return  <nav>
-  <a href="#home">Home</a>
-  <a href="#about">About</a>
- </nav>;
-  
+  // update the JSX being returned!
+  return <nav>
+    <a href="#home">Home</a>
+    <a href="#about">About</a>
+  </nav>;
 }
 
 export default NavBar;
