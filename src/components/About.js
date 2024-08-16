3b5b13579823367c9d20569307f1bea289bd8192
stanleyndonj <stanley.ndonj@student.moringaschool.com>
2024-08-16 10:08:34 +0300

    lab complete

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+        <h2>About me</h2>
+        <p>Yes this is me</p>
+        <img src={image} alt="I made this" />
+  </div>;
 }
 
 export default About;
